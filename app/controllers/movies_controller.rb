73dc9e1d7ec385ff66aa77c48e356bf3079175ca
Paula Ho <pho2@chicagobooth.edu>
2023-04-25 00:40:48 +0000

    Finished Director and Movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,15 @@ class MoviesController < ApplicationController
 
   def index
     @list_of_movies = Movie.all
-
+   
     render({ :template => "movie_templates/index.html.erb"})
+  end
+
+  def movie_details
+    #params looks like {:a_movie=>"1"}
+    the_movie = params.fetch("a_movie")
+    @the_movie = Movie.where({ :id => the_movie }).at(0)
+    @director = Director.where({ :movie_id => @the_movie.id })
+    render({ :template => "movie_templates/show.html.erb"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
+  #homepage
   get("/", { :controller => "application", :action => "homepage" })
 
+  #directors
   get("/directors", { :controller => "directors", :action => "index" })
   get("/directors/youngest", { :controller => "directors", :action => "youngest_director" })
   get("/directors/eldest", { :controller => "directors", :action => "eldest_director" })
   get("/directors/:an_id", { :controller => "directors", :action => "director_details" })
 
+  #movies
   get("/movies", { :controller => "movies", :action => "index" })
+  get("/movies/:a_movie", { :controller => "movies", :action => "movie_details" })
 
+  #actors
   get("/actors", { :controller => "actors", :action => "index" })
 end
